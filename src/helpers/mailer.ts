4a8c30c23aebdbf496f6e8bda3c5ba31821dfebe
ParthGupta84616 <parthguptaji20@gmail.com>
2024-07-08 20:32:02 +0530

    setup mailer userModel dbConfig

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,32 @@
+import { Html } from 'next/document';
+import nodemailer from 'nodemailer';
+
+export const sendMail = async ({email, emailType , userId}:{email:string, emailType:string , userId:string} ) => {
+    try {
+        const transporter = nodemailer.createTransport({
+          host: process.env.SMTP_HOST,
+          port: 465,
+          secure: true,
+          auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASSWORD,
+          },
+        });
+      
+        const mailOptions = {
+          from: process.env.SMTP_FROM,
+          to: email,
+          subject: emailType === 'VERIFY' ? 'Verify your email' : 'Reset your password',
+          // text : emailType === 'VERIFY' ? `Click on the link to verify your email: http://localhost:3000/verify/${userId}` : `Click on the link to reset your password: http://localhost:3000/reset-password/${userId}`,
+          // text : emailType === 'VERIFY' ? `Click on the link to verify your email: ${process.env.CLIENT_URL}/verify/${userId}` : `Click on the link to reset your password: ${process.env.CLIENT_URL}/reset-password/${userId}`,
+          html : "<h1>Test</h1>"
+        };
+      
+        return await transporter.sendMail(mailOptions);
+      
+      }
+       catch (error: any) {
+        throw new Error(error.message);
+        
+    }
+}
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -5,5 +5,30 @@ const userSchema = new mongoose.Schema({
         type : String,
         required : [true, 'Please provide a username'],
         unique : [true, 'Username already exists']
-    }
+    },
+    email: {
+        type : String,
+        required : [true, 'Please provide a email'],
+        unique : [true, 'email already exists']
+    },
+    password: {
+        type : String,
+        required : [true, 'Please provide a password']
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    resetPasswordToken: String,
+    resetPasswordTokenExpire: Date,
+    verificationToken: String,
+    verificationTokenExpire: Date
 });
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
